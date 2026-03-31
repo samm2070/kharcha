efbcf9d21c4b3341669cd6de9f3d46fabdc4e116
sam <95059618+samm2070@users.noreply.github.com>
2026-03-31 11:31:37 +0530

    Update service worker to version 2 and simplify fetch

--- sw.js
+++ sw.js
@@ -1,26 +1,16 @@
-const CACHE = 'kharcha-v1';
-const ASSETS = ['/kharcha/', '/kharcha/index.html', '/kharcha/manifest.json'];
+const CACHE = 'kharcha-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      if (res && res.status === 200 && e.request.method === 'GET') {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match('/index.html')))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
